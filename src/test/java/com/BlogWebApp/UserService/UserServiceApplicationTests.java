593d80c1c41f74561c609ee2714245fbf8949aee
Eylem Abuzeyitoglu <166799047+eylemabuzeyitoglu@users.noreply.github.com>
2025-04-19 17:32:41 +0300

    Initial commit

---
+++ src/test/java/com/BlogWebApp/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlogWebApp.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
